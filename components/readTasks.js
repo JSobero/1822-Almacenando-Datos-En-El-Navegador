3478d088c7d30f2e78f398f43eab8d96f47a0987
JSobero <joaquinpomayay2020@gmail.com>
2022-12-27 21:00:16 -0500

    clase05 completada

---
+++ components/readTasks.js
@@ -0,0 +1,22 @@
+import { createTask } from './addTasks.js';
+import { uniqueDates, orderDates } from '../services/date.js';
+import dateElement from './dateElement.js';
+
+export const displayTasks = () => {
+    const list = document.querySelector('[data-list]');
+
+    const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+    const dates = uniqueDates(taskList);
+    const oder = orderDates(dates);
+    dates.forEach( date => {
+        const dateMoment = moment(date, 'DD/MM/YYYY');
+        list.appendChild(dateElement(date));
+        taskList.forEach((task) => {
+            const taskDate = moment(task.dateFormat, 'DD/MM/YYYY');
+            const diff = dateMoment.diff(taskDate);
+            if (diff == 0){
+                list.appendChild(createTask(task));
+            }
+        });
+    });
+};
